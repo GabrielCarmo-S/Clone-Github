ceea583c1aba00096417ef3516d38d9ef3513669
Gabriel Carmo <perien.op@gmail.com>
2021-02-09 14:14:20 -0300

    Criado Profile da pagina

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide} from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'GabrielCarmo-S'}
+            name={'Gabriel Carmo'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/70289493?v=4'}
+            followers={14}
+            following={25}
+            company={'TIM'}
+            location={'São Paulo, Brazil'}
+            email={'gabriekcarmk@gmail.com'}
+            blog={'linkedin.com/in/gabrielcarmo-s'}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
