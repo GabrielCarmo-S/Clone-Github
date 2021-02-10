4ccbda32fa8bacef0a919f38b7d586b150c1222c
Gabriel Carmo <perien.op@gmail.com>
2021-02-10 16:12:17 -0300

    Updates

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  BreadCrumb,
+  RepoIcon,
+  Stars,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <BreadCrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/gabrielcarmo-s"}>
+          gabrielcarmo-s
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/gabrielcarmo-s/Happy"}>
+          Happy
+        </Link>
+      </BreadCrumb>
+
+      <p>Contains all</p>
+
+      <Stars>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stars>
+
+      <LinkButton href={'https://github.com/gabrielcarmo-s/Happy'}>
+        <GithubIcon />
+        <span>View on github</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
