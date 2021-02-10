fc9d1d6f2ba4d4c8bec9d258ad7a1d90c840b72e
Gabriel Carmo <perien.op@gmail.com>
2021-02-10 15:41:36 -0300

    Atualizado a estilização da pagina

--- src/components/RandomCalendar/index.tsx
+++ src/components/RandomCalendar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Heatmap from "react-calendar-heatmap";
-import { subYears } from "date-fns";
+import { subYears, isBefore, isSameDay, addDays } from "date-fns";
 
 import { Container } from "./styles";
 
@@ -10,29 +10,24 @@ const RandomCalendar: React.FC = () => {
   const starDate = subYears(new Date(), 1);
   const endDate = new Date();
 
-  const values: HeatmapValue[] = [];
-
-  values.push({ date: new Date(), count: 3 });
-
   return (
     <Container>
       <div className="wrapper">
         <Heatmap
           startDate={starDate}
           endDate={endDate}
-          values={[values]}
+          values={generateHeatmapValues(starDate, endDate)}
           gutterSize={3.5}
           classForValue={(item: HeatmapValue) => {
             let clampedCount = 0;
 
-            if (item != null){
+            if (item != null) {
               clampedCount = Math.max(item.count, 0);
               clampedCount = Math.min(item.count, 4);
             }
 
             return `scale-${clampedCount}`;
           }}
-
           showWeekdayLabels
         />
       </div>
@@ -40,6 +35,21 @@ const RandomCalendar: React.FC = () => {
       <span>Random calendar (do not represent actual data)</span>
     </Container>
   );
+};
+
+const generateHeatmapValues = (startDate: Date, endDate: Date) => {
+  const values: HeatmapValue[] = [];
+
+  let currentDate = startDate;
+  while (isBefore(currentDate, endDate) || isSameDay(currentDate, endDate)) {
+    const count = Math.random() * 4;
+
+    values.push({ date: currentDate, count: Math.round(count) });
+
+    currentDate = addDays(currentDate, 1);
+  }
+
+  return values;
 };
 
 export default RandomCalendar;
