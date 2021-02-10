23f7c298f041b0c28428a29465a65c0da009d131
Gabriel Carmo <perien.op@gmail.com>
2021-02-10 16:22:23 -0300

    Updates

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -4,9 +4,47 @@ import { AiOutlineFork } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+
+  > p {
+    font-size: 16px;
+  }
 `;
 
-export const BreadCrumb = styled.div``;
+export const BreadCrumb = styled.div`
+  margin-bottom: 16px;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+
+  font-size: 18px;
+
+  > a {
+    color: var(--link);
+    text-decoration: none;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+
+    &.username {
+      margin-left: 8px;
+    }
+
+    &.reponame {
+      font-weight: 600;
+    }
+  }
+
+  > span {
+    padding: 0 5px;
+  }
+`;
 
 const iconCSS = css`
   width: 16px;
@@ -19,7 +57,22 @@ export const RepoIcon = styled(RiBookMarkLine)`
   ${iconCSS}
 `;
 
-export const Stars = styled.div``;
+export const Stars = styled.ul`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+
+  > li {
+    display: flex;
+    align-items: center;
+    margin-right: 9px;
+
+    > * {
+      margin-right: 7px;
+      color: var(--gray);
+    }
+  }
+`;
 
 export const StarIcon = styled(RiStarLine)`
   ${iconCSS}
@@ -29,7 +82,26 @@ export const ForkIcon = styled(AiOutlineFork)`
   ${iconCSS}
 `;
 
-export const LinkButton = styled.a``;
+export const LinkButton = styled.a`
+  text-decoration: none;
+
+  margin-top: 24px;
+  background: var(--gray-dark);
+  padding: 12px 17px;
+  border-radius: 24px;
+  width: max-content;
+
+  display: flex;
+  align-items: center;
+
+  > span {
+    color: var(--primary);
+  }
+  > svg {
+    fill: var(--primary);
+    margin-right: 10px;
+  }
+`;
 
 export const GithubIcon = styled(FaGithub)`
   ${iconCSS}
